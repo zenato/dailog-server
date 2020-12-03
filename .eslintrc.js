137126b9af7d89690a40299e6047fad53a5f8f18
Yeongjin Lee <yeongjinnn@gmail.com>
2020-12-03 09:02:03 +0900

    Disabled explicit function return type warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,13 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-explicit-any': 0,
-    'prettier/prettier': ['error', {
-      'semi': false
-    }]
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+      },
+    ],
   },
 }
